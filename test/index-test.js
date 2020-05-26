d936dbe525957cac3eaf98040d40523de580ec14
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-05-26 15:45:46 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,7 +8,7 @@ describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it('calls console.error(Please Help!)', done => {
+  it('calls console.error("Please Help!")', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
@@ -20,7 +20,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.log(Just loggin)', done => {
+  it('calls console.log("Just loggin")', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
     jsdom.env(html, [src], {
@@ -32,7 +32,7 @@ describe('index', () => {
     })
   })
 
-  it('calls console.warn(Could be trouble!)', done => {
+  it('calls console.warn("Could be trouble!")', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
 
     jsdom.env(html, [src], {
